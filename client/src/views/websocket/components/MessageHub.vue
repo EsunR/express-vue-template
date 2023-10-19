e359ceed57cd2a67b7ce822047bae281b225c66b
EsunR <esunr641@icloud.com>
2023-10-19 16:48:30 +0800

    fix: 修复websocket监听没有注销的问题

--- client/src/views/websocket/components/MessageHub.vue
+++ client/src/views/websocket/components/MessageHub.vue
@@ -1,25 +1,46 @@
 <script lang="ts" setup>
 import { ElMessageBox } from 'element-plus';
 import useMessageWs from '../hooks/useMessageWs';
+import { MessageServerToClientEvents } from '@express-vue-template/types/ws';
 
 defineOptions({
   name: 'MessageHub',
 });
 
 const messageWsState = useMessageWs();
-
 const chatMessageHub = ref<string[]>([]);
 
-messageWsState.socket.value?.on('chat message', (msg: string) => {
+const onListenChatMessage: MessageServerToClientEvents['chat message'] = (
+  msg
+) => {
   chatMessageHub.value.push(msg);
-});
+};
 
-messageWsState.socket.value?.on('broadcast message', (msg: string) => {
-  ElMessageBox.alert(msg, 'Broadcast Message');
-});
+const onListenBroadcastMessage: MessageServerToClientEvents['broadcast message'] =
+  (msg) => {
+    ElMessageBox.alert(msg, 'Broadcast Message');
+  };
 
-messageWsState.socket.value?.on('disconnect', () => {
+const onListenDisconnect = () => {
   chatMessageHub.value = [];
+};
+
+onMounted(() => {
+  messageWsState.socket.value?.on('chat message', onListenChatMessage);
+  messageWsState.socket.value?.on(
+    'broadcast message',
+    onListenBroadcastMessage
+  );
+  messageWsState.socket.value?.on('disconnect', onListenDisconnect);
+});
+
+onUnmounted(() => {
+  messageWsState.socket.value?.off('chat message', onListenChatMessage);
+  messageWsState.socket.value?.off(
+    'broadcast message',
+    onListenBroadcastMessage
+  );
+  messageWsState.socket.value?.off('disconnect', onListenDisconnect);
 });
 </script>
 
--- client/src/views/websocket/index.vue
+++ client/src/views/websocket/index.vue
@@ -1,10 +1,21 @@
 <script lang="ts" setup>
-import Connection from './components/Connection.vue';
 import ChatInput from './components/ChatInput.vue';
+import Connection from './components/Connection.vue';
 import MessageHub from './components/MessageHub.vue';
+import useMessageWs from './hooks/useMessageWs';
 
 defineOptions({
   name: 'Websocket',
+});
+
+const { connect, disconnect } = useMessageWs();
+
+onMounted(() => {
+  connect();
+});
+
+onUnmounted(() => {
+  disconnect();
 });
 </script>
 
